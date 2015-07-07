5e561b14b0ca172b8b2f25575329ae782f33de35
Roberto Osorio-Goenaga <r.osorio@samsung.com>
2015-07-07 13:37:27 -0400

    Initial Commit

---
+++ SmartWordToy/SmartWordToy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SmartWordToy
+//
+//  Created by Roberto Osorio-Goenaga on 7/7/15.
+//  Copyright (c) 2015 Roberto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
